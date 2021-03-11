2cda2266f78df8131b1c843907544abfc87e4442
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:45:07 +0800

    Create SideAd.vue

---
+++ src/components/card/SideAd.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-card class="box-card">
+    <div class="ad-div">
+      <el-tag class="ad-tag" type="info" size="small" effect="plain">广告</el-tag>
+      <div v-html="$root.site_info.site_side_ad"></div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "SideAd"
+}
+</script>
+
+<style lang="stylus" scoped>
+.ad-div
+  position relative
+
+.ad-tag
+  position absolute
+  right 0
+  bottom 0
+
+</style>

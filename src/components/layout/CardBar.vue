2f437c1fc96b89e7d92d2e728136fac7167e1a19
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:45:48 +0800

    Create CardBar.vue

---
+++ src/components/layout/CardBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <Welcome></Welcome>
+    <Tips></Tips>
+    <Links></Links>
+    <SideAd></SideAd>
+  </section>
+</template>
+
+<script>
+import Welcome from '@/components/card/Welcome'
+import Tips from '@/components/card/Tips'
+import Links from '@/components/card/Links'
+import SideAd from '@/components/card/SideAd'
+
+export default {
+  name: "CardBar",
+  components: {
+    Welcome,
+    Tips,
+    Links,
+    SideAd
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

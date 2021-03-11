c23fe77a0af5883a94573a5ef796ddae4188a880
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:45:29 +0800

    Update ReActive.vue

--- src/views/ReActive.vue
+++ src/views/ReActive.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="ReActive">
+  <div id="reactive">
     <h1>重新激活</h1>
   </div>
 </template>
 
 <script>
 export default {
-name: "ReActive"
+  name: "ReActive"
 }
 </script>
 

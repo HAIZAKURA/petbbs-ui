4e0cae3ed09fc573570fdcd7dd320ef481012394
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:44:58 +0800

    Create notify.js

---
+++ src/api/notify.js
@@ -0,0 +1,20 @@
+import request from '@/utils/request'
+
+// 获取未读通知
+export function getNewNotify() {
+  return request({
+    url: '/api/notify/new',
+    method: 'get'
+  })
+}
+
+// 获取所有通知
+export function getAllNotify() {
+  return request({
+    url: '/api/notify',
+    method: 'get'
+  })
+}
+
+// 已读通知
+export function setReadNotify() {}

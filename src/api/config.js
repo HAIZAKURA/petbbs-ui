9b7cb0afac80b20ec06a5d037afc02dba0e139f3
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 11:37:41 +0800

    Update Config

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,79 @@
 import request from '@/utils/request'
 
+// 获取网站信息
 export function getSiteInfo() {
   return request({
     url: '/api/info',
     method: 'get'
+  })
+}
+
+// 获取设置
+export function getSiteConfig() {
+  return request({
+    url: '/api/config',
+    method: 'get'
+  })
+}
+
+// 修改设置
+export function updateSiteConfig(configs) {
+  return request({
+    url: '/api/config',
+    method: 'put',
+    data: configs
+  })
+}
+
+// email测试
+export function mailTest(mailto) {
+  return request({
+    url: '/api/mail_test',
+    method: 'get',
+    params: {
+      mailto: mailto
+    }
+  })
+}
+
+// 获取七牛配置
+export function getQiniuConfig() {
+  return request({
+    url: '/api/admin/qiniu',
+    method: 'get'
+  })
+}
+
+// 修改七牛配置
+export function updateQiniuConfig(QiniuDTO) {
+  return request({
+    url: '/api/admin/qiniu',
+    method: 'put',
+    data: QiniuDTO
+  })
+}
+
+// 获取敏感词列表
+export function getSensitiveWord() {
+  return request({
+    url: '/api/admin/word',
+    method: 'get'
+  })
+}
+
+// 添加敏感词
+export function addSensitiveWord(words) {
+  return request({
+    url: '/api/admin/word',
+    method: 'post',
+    data: words
+  })
+}
+
+// 删除敏感词
+export function delSensitiveWord(word) {
+  return request({
+    url: '/api/admin/word/' + word,
+    method: 'delete'
   })
 }

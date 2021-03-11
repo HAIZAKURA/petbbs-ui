9c343306b9835b6d4533c3e8d4c496371566f74d
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:45:21 +0800

    Update Forum.vue

--- src/views/Forum.vue
+++ src/views/Forum.vue
@@ -1,21 +1,91 @@
 <template>
-  <div class="Forum"></div>
+  <div id="forum">
+    <div v-if="billboard != null">
+      <p v-for="(item, key) in billboard" class="box" v-bind:key="key">🔔 公告：{{ item.content }}</p>
+    </div>
+
+    <div class="home-panel">
+      <Panel></Panel>
+    </div>
+
+    <div class="columns">
+      <div class="column is-three-quarters">
+        <PostList></PostList>
+      </div>
+
+      <div class="column">
+        <CardBar></CardBar>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { getSiteInfo } from "@/api/config";
+import Panel from '@/components/layout/Panel'
+import PostList from '@/components/post/PostList'
+import CardBar from '@/components/layout/CardBar'
+
+import { getSiteInfo } from '@/api/config'
+import { activeUser } from '@/api/user'
+import { getBillboard } from '@/api/billboard'
 
 export default {
   name: "Forum",
+  components: {
+    Panel,
+    PostList,
+    CardBar
+  },
+  data() {
+    return {
+      billboard: '',
+      activeDTO: {
+        user: this.$route.query.user,
+        code: this.$route.query.code
+      }
+    }
+  },
   created() {
     getSiteInfo().then(data => {
       this.$root.site_info = data.data
       document.title = data.data.site_title
     })
+    this.handleActive()
+    this.fetchBillboard()
+  },
+  methods: {
+    handleActive() {
+      if (this.activeDTO.code && this.activeDTO.user) {
+        activeUser(this.active).then((value) => {
+          let { code, message } = value
+          if (code === 200) {
+            this.$message({
+              message: message,
+              type: 'success',
+              duration: 2000
+            })
+          } else {
+            this.$message({
+              message: message,
+              type: 'error',
+              duration: 2000
+            })
+          }
+        })
+      }
+    },
+    async fetchBillboard() {
+      getBillboard().then((data) => {
+        this.billboard = data.data
+      })
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+.home-panel
+  margin-top 1em
+  margin-bottom 0.25em
 
 </style>

fe22878fc2a81ad8292c2ce77984d12bb1c45230
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:45:24 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Login">
+  <div id="login">
     <h1>用户登录</h1>
     <div>
       <el-form
@@ -44,58 +44,58 @@ export default {
       redirect: undefined,
       loading: false,
       ruleForm: {
-        username: "",
-        password: "",
+        username: '',
+        password: '',
         rememberMe: false
       },
       rules: {
         username: [
           {
             required: true,
-            message: "请输入用户名",
-            trigger: "blur"
+            message: '请输入用户名',
+            trigger: 'blur'
           },
           {
             min: 3,
             max: 15,
-            message: "用户名长度应为3-15",
-            trigger: "blur"
+            message: '用户名长度应为3-15',
+            trigger: 'blur'
           }
         ],
         password: [
           {
             required: true,
-            message: "请输入密码",
-            trigger: "blur"
+            message: '请输入密码',
+            trigger: 'blur'
           },
           {
             min: 6,
             max: 20,
-            message: "密码长度应为6-20",
-            trigger: "blur"
+            message: '密码长度应为6-20',
+            trigger: 'blur'
           }
         ]
       }
     }
   },
   created() {
-    document.title = "登录"
+    document.title = '登录'
   },
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
           this.loading = true;
-          this.$store.dispatch("user/login", this.ruleForm)
+          this.$store.dispatch('user/login', this.ruleForm)
               .then(() => {
                 this.$message({
-                  message: "登录成功",
-                  type: "success",
+                  message: '登录成功',
+                  type: 'success',
                   duration: 2000
                 })
                 setTimeout(() => {
                   this.loading = false;
-                  this.$router.push({ path: this.redirect || "/" })
+                  this.$router.push({ path: this.redirect || '/' })
                 }, 0.1 * 1000)
               })
               .catch(() => {

3156911470b641b79471cdb4c361bc34a4215a6e
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:45:36 +0800

    Create PostBox.vue

---
+++ src/components/post/PostBox.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="post-box" :postInfo="postInfo">
+    <el-row :gutter="10">
+      <el-col :span="2">
+        <img class="post-box-avatar" :src="postInfo.avatar + '?imageView2/1/w/100/h/100/format/webp/q/80'" :alt="postInfo.alias" />
+      </el-col>
+      <el-col :span="19">
+        <div class="post-box-info">
+          <div class="post-title">
+            <router-link class="title-link" :to="{ path: '/' }">{{ postInfo.title }}</router-link>
+          </div>
+          <div class="post-info">
+            <p>
+              <span>
+                <router-link style="font-weight: 600;color:#606266" :to="{ path: '/' }">{{ postInfo.alias }}</router-link>
+              </span>
+              <span style="color: #C0C4CC">  •  </span>
+              <span>{{ dayjs(postInfo.createTime).calendar() }}</span>
+              <span v-for="(tag, index) in postInfo.tags" :key="index">
+                <span style="color: #C0C4CC">  •  </span>
+                <el-tag type="info" size="mini">
+                  <router-link style="color:#606266" :to="{ path: '/tags/' + tag.name }"><i class="fa fa-tag"></i>&nbsp;{{ tag.name }}</router-link>
+                </el-tag>
+              </span>
+            </p>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="post-box-comments">
+          <p style="font-weight: bold"><el-tag type="info" size="small" effect="plain"><i class="fa fa-commenting"></i>&nbsp;{{ postInfo.comments }}</el-tag></p>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostBox",
+  props: {
+    postInfo: {
+      require: true
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.post-box
+  width 100%
+  height 80px
+  padding 8px 0
+
+.post-box-avatar
+  height 48px
+  width 48px
+  border-radius 50%
+  overflow hidden
+  margin-top 6px
+
+.post-box-info
+  height 48px
+  overflow hidden
+  margin-top 6px
+
+.post-box-comments
+  height 48px
+  overflow hidden
+  margin-top 6px
+  p
+    line-height 60px
+    text-align right
+
+.post-title
+  height 24px
+
+.post-info
+  height 28px
+  p
+    line-height 28px
+
+.title-link
+  line-height 24px
+  font-size 16px
+  color #606266
+  font-weight 500
+
+</style>

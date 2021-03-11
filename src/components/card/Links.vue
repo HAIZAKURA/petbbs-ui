831592cb41612f1f74411f243805771febf3f7cc
HAIZAKURA <hhbilly99@gmail.com>
2021-03-11 22:45:05 +0800

    Create Links.vue

---
+++ src/components/card/Links.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header">
+      <span>👬 友情链接</span>
+    </div>
+    <div>
+      <div class="link-div" v-for="(item, key) in friendLinks" :key="key">
+        <el-link :underline="false" :href="item.link" :alt="item.title" target="_blank">
+          <img class="link-img" :src="item.icon" :alt="item.title" />
+        </el-link>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+import { getFriends } from '@/api/friends'
+
+export default {
+  name: "Links",
+  data() {
+    return {
+      friendLinks: []
+    }
+  },
+  created() {
+    this.fetchFriendLinks()
+  },
+  methods: {
+    fetchFriendLinks() {
+      getFriends().then((data) => {
+        this.friendLinks = data.data
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.link-div
+  height 30px
+  text-align center
+
+.link-img
+  height 30px
+</style>

eda9f021304e5106de887c04e8671f336758b36e
HAIZAKURA <hhbilly99@gmail.com>
2021-04-06 16:08:18 +0800

    Update QiniuConfig.vue

--- src/views/admin/QiniuConfig.vue
+++ src/views/admin/QiniuConfig.vue
@@ -5,12 +5,81 @@
     </div>
 
     <el-divider></el-divider>
+
+    <div class="qiniuFormDiv">
+      <el-form ref="qiniuForm" label-position="left" :model="qiniuConfig" label-width="100px">
+        <el-form-item label="Access Key">
+          <el-input v-model="qiniuConfig.accesskey"></el-input>
+        </el-form-item>
+
+        <el-form-item label="Secret Key">
+          <el-input v-model="qiniuConfig.secretkey"></el-input>
+        </el-form-item>
+
+        <el-form-item label="Bucket">
+          <el-input v-model="qiniuConfig.bucket"></el-input>
+        </el-form-item>
+
+        <el-form-item label="Domain">
+          <el-input v-model="qiniuConfig.host"></el-input>
+        </el-form-item>
+
+        <el-form-item label="SSL">
+          <el-checkbox v-model="qiniuConfig.usessl"></el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="upQiniuConfig">修改</el-button>
+
+          <span class="mx-4"></span>
+          <a href="https://portal.qiniu.com/user/key" target="_blank">
+            <span>如何申请？</span>
+          </a>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
+import { getQiniuConfig, updateQiniuConfig } from '@/api/config'
+
 export default {
-  name: "QiniuConfig"
+  name: "QiniuConfig",
+  data() {
+    return {
+      qiniuConfig: {
+        accesskey: '',
+        secretkey: '',
+        bucket: '',
+        host: '',
+        usessl: false
+      }
+    }
+  },
+  created() {
+    this.fetchQiniuConfig()
+  },
+  methods: {
+    async fetchQiniuConfig() {
+      getQiniuConfig().then((res) => {
+        let { data } = res
+        this.qiniuConfig.accesskey = data.accesskey
+        this.qiniuConfig.secretkey = data.secretkey
+        this.qiniuConfig.bucket = data.bucket
+        this.qiniuConfig.host = data.host
+        this.qiniuConfig.usessl = data.usessl
+      })
+    },
+    upQiniuConfig() {
+      updateQiniuConfig(this.qiniuConfig).then(() => {
+        this.$message({
+          message: '修改成功',
+          type: 'success'
+        })
+      })
+    }
+  }
 }
 </script>
 

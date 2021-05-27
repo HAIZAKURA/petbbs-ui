b1413a891bd3ac5b10bbcdef261ea685ceb2a58b
HAIZAKURA <hhbilly99@gmail.com>
2021-05-27 22:00:50 +0800

    Update Panel.vue

--- src/components/layout/Panel.vue
+++ src/components/layout/Panel.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="columns is-hidden-mobile is-multiline has-text-centered">
+  <div class="columns is-hidden-mobile is-multiline has-text-centered mb-2">
     <div class="column is-one-quarter">
-      <router-link :to="{path:'/tags'}">
-        <el-tooltip content="所有标签" placement="bottom">
+      <router-link :to="{path:'/tags/1384720738488451074?name=狗狗'}">
+        <el-tooltip content="狗狗" placement="bottom">
           <section class="box my-card hero is-warning">
-            <i class="fas fa-tags fa-4x"></i>
+            <i class="fad fa-dog fa-4x"></i>
           </section>
         </el-tooltip>
       </router-link>
     </div>
 
     <div class="column is-one-quarter">
-      <router-link :to="{ path: '/sections' }">
-        <el-tooltip content="优质专栏" placement="bottom">
+      <router-link :to="{ path: '/tags/1384720738450702338?name=猫猫' }">
+        <el-tooltip content="猫猫" placement="bottom">
           <section class="box my-card hero is-info">
-            <i class="fas fa-list-alt fa-4x"></i>
+            <i class="fas fa-cat fa-4x"></i>
           </section>
         </el-tooltip>
       </router-link>
     </div>
 
     <div class="column is-one-quarter">
-      <router-link :to="{path:'/photos'}">
-        <el-tooltip content="萌宠照片墙" placement="bottom">
+      <router-link :to="{path:'/tags/1397563954061434882?name=鸭鸭'}">
+        <el-tooltip content="鸭鸭" placement="bottom">
           <section class="box my-card hero is-primary">
-            <i class="fas fa-camera fa-4x"></i>
+            <i class="fad fa-duck fa-4x"></i>
           </section>
         </el-tooltip>
       </router-link>
     </div>
 
     <div class="column is-one-quarter">
-      <router-link :to="{path:'/random'}">
-        <el-tooltip content="随机推荐" placement="bottom">
-          <section class="box my-card hero is-danger">
-            <i class="fas fa-paw fa-4x"></i>
+      <router-link :to="{path: '/tags/1397565339259691009?name=龟龟'}">
+        <el-tooltip content="龟龟" placement="bottom">
+          <section class="box my-card hero is-success">
+            <i class="fas fa-turtle fa-4x"></i>
           </section>
         </el-tooltip>
       </router-link>

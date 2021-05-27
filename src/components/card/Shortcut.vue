671444e2c02f2e1988f5655c9742cbf2bf756bcf
HAIZAKURA <hhbilly99@gmail.com>
2021-05-27 22:00:40 +0800

    Create Shortcut.vue

---
+++ src/components/card/Shortcut.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="columns shortcut has-text-centered">
+    <div class="column is-half">
+      <router-link :to="{path:'/sections'}">
+        <el-tooltip content="优质专栏" placement="bottom">
+          <section class="box my-card hero is-dark">
+            <i class="fad fa-pencil-paintbrush fa-2x"></i>
+<!--            <span class="subtitle mt-2">优质专栏</span>-->
+          </section>
+        </el-tooltip>
+      </router-link>
+    </div>
+
+    <div class="column is-half">
+      <router-link :to="{path:'/photos'}">
+        <el-tooltip content="照片墙" placement="bottom">
+          <section class="box my-card hero is-danger">
+            <i class="far fa-images fa-2x"></i>
+<!--            <span class="subtitle mt-2">照片墙</span>-->
+          </section>
+        </el-tooltip>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Shortcut"
+}
+</script>
+
+<style lang="stylus" scoped>
+.subtitle
+  font-size 1em
+</style>

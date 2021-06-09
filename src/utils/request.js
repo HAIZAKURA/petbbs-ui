ae39989336a15c892b47970758ba2760d34896ea
HAIZAKURA <hhbilly99@gmail.com>
2021-06-09 18:30:49 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,23 +7,26 @@ import store from '@/store'
 const service = axios.create({
   // 公共接口 url = base url + request url
   baseURL: process.env.VUE_APP_SERVER_URL,
-
   // baseURL: 'https://api.example.com',
+
   // 超时时间 ms
   timeout: 30 * 1000
 })
 
-// 设置cross跨域 并设置访问权限 允许跨域携带cookie信息,使用JWT可关闭
+// 设置cross跨域 并设置访问权限
+// 跨域携带cookie信息 使用JWT关闭
 service.defaults.withCredentials = false
 
 // 请求拦截器 request interceptor
 service.interceptors.request.use(
   config => {
-    // 发请求前做的一些处理，数据转化，配置请求头，设置token,设置loading等，根据需求去添加
-    // 注意使用token的时候需要引入cookie方法或者用本地localStorage等方法，推荐js-cookie
+    // 发请求前处理 数据转化 配置请求头 设置token 设置loading
+    // 使用token 本地localStorage
     if (store.getters.token) {
-      // config.params = {'token': token}    // 如果要求携带在参数中
-      // config.headers.token = token;       // 如果要求携带在请求头中
+      // config.params = {'token': token}
+      // 如果要求携带在参数中
+      // config.headers.token = token;
+      // 如果要求携带在请求头中
       // bearer：w3c规范
       config.headers['Authorization'] = 'Bearer ' + getToken()
     }
@@ -38,18 +41,18 @@ service.interceptors.request.use(
 
 // 响应拦截器
 service.interceptors.response.use(
-  // 接收到响应数据并成功后的一些共有的处理，关闭loading等
+  // 接收到响应数据后处理
   response => {
     const res = response.data
-    // 如果自定义代码不是200，则将其判断为错误。
+    // 如果代码不是200 报错
     if (res.code !== 200) {
-      // 50008: 非法Token
-      // 50012: 异地登录
-      // 50014: Token失效
+      // 50008 非法Token
+      // 50012 异地登录
+      // 50014 Token失效
       if (res.code === 401 || res.code === 50012 || res.code === 50014) {
         this.$store.dispatch('user/logout')
         // 重新登录
-        MessageBox.confirm('会话失效，您可以留在当前页面，或重新登录', '权限不足', {
+        MessageBox.confirm('会话已失效，您可以留在当前页面，或重新登录', '权限不足', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning',
@@ -58,7 +61,8 @@ service.interceptors.response.use(
           window.location.href = '#/login'
         })
       }
-      else { // 其他异常直接提示
+      else {
+        // 其他异常直接提示
         Message({
           showClose: true,
           message: res.message || 'Error',
@@ -73,7 +77,7 @@ service.interceptors.response.use(
     }
   },
   error => {
-    /** *** 接收到异常响应的处理开始 *****/
+    // 接收到异常响应处理开
     // console.log('err' + error)
     Message({
       showClose: true,
